9ae44944c552ce469eb3caea66003f471abffcb0
Loïc Nogues <nogues.loic@gmail.com>
2018-04-11 12:02:01 +0200

    Decode XML

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const hamsterRouter = require('./routes/hamster');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hamster', hamsterRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/hamster.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const multer  = require('multer');
+const libXML = require('libxmljs');
+const cheerio = require('cheerio');
+
+const router = express.Router();
+const upload = multer();
+
+const parseXml = xml => {
+  const $ = cheerio.load(xml, { xmlMode: true });
+  let activities = [];
+
+  $('activity').each(function(i, el) {
+    activities.push({
+      category: $(this).attr('category'),
+      description: $(this).attr('description'),
+      durationMinutes: $(this).attr('duration_minutes'),
+      startTime: $(this).attr('start_time'),
+      endTime: $(this).attr('end_time'),
+      name: $(this).attr('name'),
+      tags: $(this).attr('tags'),
+    });
+  });
+
+  return activities;
+};
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('hamster');
+});
+
+router.post('/', upload.single('xml'), (req, res, next) => {
+  const file = req.file;
+
+  if (file) {
+    const xml = Buffer.from(file.buffer, file.encoding).toString('utf8');
+    const data = parseXml(xml);
+
+    console.log(data);
+  }
+});
+
+module.exports = router;
