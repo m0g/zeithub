ac361ff9bcfbf8aaf7c8761b35f10a0b686c7924
Loïc Nogues <nogues.loic@gmail.com>
2018-04-12 17:43:48 +0200

    Added link

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/projects">Projects</router-link>
     <router-view></router-view>
   </div>
 </template>
