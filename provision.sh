04338d67b78bd05ad92f05acedf1c0ecefe1a231
Loïc Nogues <nogues.loic@gmail.com>
2018-04-19 15:33:53 +0200

    Updated node

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@ export LANGUAGE="en_US.UTF-8"
 echo "LANGUAGE="en_US.UTF-8"" | sudo tee --append /etc/default/locale
 echo "LC_ALL="en_US.UTF-8"" | sudo tee --append /etc/default/locale
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
 sudo apt-get update
 sudo apt-get dist-upgrade -y
