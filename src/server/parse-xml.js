eae74466e2a49f5ec17924486bc643ec9eb93fa5
Loïc Nogues <nogues.loic@gmail.com>
2018-05-16 18:37:45 +0200

    Restored missing parse-xml.js

--- src/server/parse-xml.js
+++ src/server/parse-xml.js
@@ -1,19 +1,21 @@
-module.exports = (uri, options = {}) => {
-  const urlString = uri.match(/^http/) ? uri : `${location.origin}${uri}`;
-  const token = localStorage.getItem('token');
-  const url = new URL(urlString);
+const cheerio = require('cheerio');
 
-  if (!options.headers) {
-    options.headers = {};
-  }
+module.exports = xml => {
+  const $ = cheerio.load(xml, { xmlMode: true });
+  let activities = [];
 
-  if (token) {
-    options.headers['Authorization'] = `Bearer ${token}`;
-  }
+  $('activity').each(function() {
+    activities.push({
+      category: $(this).attr('category'),
+      description: $(this).attr('description').replace(/'/g, "\\'"),
+      durationMinutes: parseInt($(this).attr('duration_minutes')),
+      startTime: $(this).attr('start_time'),
+      endTime: $(this).attr('end_time'),
+      name: $(this).attr('name').replace(/'/g, "\\'"),
+      tags: $(this).attr('tags'), // TODO: this should be split
+    });
+  });
 
-  if (options.query) {
-    url.search = new URLSearchParams(options.query);
-  }
-
-  return fetch(url, options);
+  return activities
+    .filter(activity => activity.startTime && activity.endTime);
 };
