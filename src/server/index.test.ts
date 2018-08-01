28329e82ae652e594a0d1c2051903854c85c6ce2
Loïc Nogues <nogues.loic@gmail.com>
2018-08-01 15:50:48 +0200

    Test a sign up event

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -2,14 +2,6 @@ import * as assert from "assert";
 import * as request from "supertest";
 import app from "./index";
 
-describe("Array", function() {
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-
 describe("Get api/me", function() {
   this.timeout(3000);
 
@@ -21,6 +13,26 @@ describe("Get api/me", function() {
       .expect(403)
       .end(function(err) {
         console.log("end", err);
+        done();
+      });
+  });
+});
+
+describe("Sign up", function() {
+  it("should create a new user and return a token", function(done) {
+    request(app)
+      .post("/api/sign/up")
+      .send({ username: "test", password: "test123", email: "test@te.st" })
+      .set("Accept", "application/json")
+      .expect(200)
+      .end((err, res) => {
+        const { user, token } = res.body;
+
+        assert.equal(user.id, 1);
+        assert.equal(user.name, "test");
+        assert.equal(user.email, "test@te.st");
+        assert.ok(token);
+
         done();
       });
   });
