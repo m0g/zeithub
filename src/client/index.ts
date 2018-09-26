39fd1e77ea53fbea65e91a61fcaa5770afc527a3
Loïc Nogues <nogues.loic@gmail.com>
2018-09-26 15:45:40 +0200

    Duplicate filters

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,9 +1,30 @@
 import Vue from 'vue';
+import * as moment from 'moment';
 
 import App from './components/app.vue';
 import router from './router';
 
-import './../lib/filters';
+// Duplication of filters with filter.ts
+Vue.filter('formatDate', (date, format = 'MMMM Do YYYY, h:mm:ss a') =>
+  moment(date).format(format)
+);
+
+Vue.filter('currency', amount => `${parseFloat(amount).toFixed(2)}€`);
+
+Vue.filter('currencyPDF', amount => `${parseFloat(amount).toFixed(2)}`);
+
+Vue.filter('formatHours', minutes =>
+  moment()
+    .startOf('day')
+    .add(minutes, 'minutes')
+    .format('HH:mm')
+);
+
+Vue.filter(
+  'totalHours',
+  minutes =>
+    `${Math.floor(minutes / 60)}:${(minutes % 60).toString().padStart(2, '0')}`
+);
 
 new Vue({
   router,
