337adcfb334fa9221d4c47a4fa54b2f2a38d1dfd
Loïc Nogues <nogues.loic@gmail.com>
2018-11-01 11:38:22 +0100

    Added address to invoice form

--- src/client/components/generate-invoice.vue
+++ src/client/components/generate-invoice.vue
@@ -6,6 +6,17 @@
       <p><input type="text" placeholder="Title" v-model="name"></p>
       <p><input type="number" placeholder="Hourly rate" v-model="hourlyRate" /></p>
       <p>
+        <select v-model="userAddressId">
+          <option value="" disabled selected>Select an address</option>
+          <option
+            v-for="ad in addresses" 
+            :key="ad.id"
+            :value="ad.id">
+              <b>{{ad.name}}</b> {{ad.street}}, {{ad.postcode}} {{ad.city}}, {{ad.country}}
+          </option>
+        </select>
+      </p>
+      <p>
         <select v-model="iban">
           <option value="" disabled selected>Select a bank account</option>
           <option
@@ -34,27 +45,43 @@ export default class GenerateInvoice extends Vue {
   errors: string[] = [];
   name: string = '';
   hourlyRate: number = 0;
-  bankAccounts: [] = [];
+  bankAccounts: Array<Object> = [];
+  addresses: Array<Object> = [];
   iban: string = '';
+  userAddressId: string = '';
 
   created() {
     this.getBankAccounts();
+    this.getAddresses();
   }
 
-  getBankAccounts() {
-    http('/api/bank-accounts', {
+  async getBankAccounts() {
+    const response = await http('/api/bank-accounts', {
       headers: { 'Content-Type': 'application/json' },
       method: 'GET'
-    })
-      .then(response => response.json())
-      .then(response => {
-        if (response.success) {
-          this.bankAccounts = response.bankAccounts;
-        }
-      });
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      this.bankAccounts = data.bankAccounts;
+    }
   }
 
-  generate(e) {
+  async getAddresses() {
+    const response = await http('/api/addresses', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'GET'
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      this.addresses = data.addresses;
+    }
+  }
+
+  async generate(e) {
     this.errors = [];
     e.preventDefault();
 
@@ -70,6 +97,10 @@ export default class GenerateInvoice extends Vue {
       this.errors.push('You need to select a bank account');
     }
 
+    if (!this.userAddressId) {
+      this.errors.push('Address is missing');
+    }
+
     if (!this.$route.query.month) {
       this.errors.push('You need to select a month in the filter area');
     }
@@ -80,18 +111,24 @@ export default class GenerateInvoice extends Vue {
         hourlyRate: this.hourlyRate,
         projectSlug: this.$route.params.slug,
         month: this.$route.query.month,
-        iban: this.iban
+        iban: this.iban,
+        userAddressId: this.userAddressId
       };
 
-      http('/api/invoices', {
+      const response = await http('/api/invoices', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body)
-      })
-        .then(response => response.json())
-        .then(response => {
-          console.log(response);
+      });
+
+      const data = await response.json();
+
+      if (data.success && data.invoiceId) {
+        this.$router.push({
+          name: 'Invoice',
+          params: { id: data.invoiceId }
         });
+      }
     }
   }
 }
--- src/server/routes/invoices/create.ts
+++ src/server/routes/invoices/create.ts
@@ -28,84 +28,98 @@ export default async (req, res) => {
       .json({ success: false, message: "Missing bank account" });
   }
 
+  if (!req.body.userAddressId) {
+    return res
+      .status(403)
+      .json({ success: false, message: "Missing user's address" });
+  }
+
   const userId = req.userId;
   const { name, hourlyRate, projectSlug, iban, userAddressId } = req.body;
 
   const [year, month] = req.body.month.split("-");
 
-  const project = await db.queryOne(`
-    select id
-    from projects
-    where user_id = ${userId} and slug = '${projectSlug}'
-  `);
+  try {
+    const project = await db.queryOne(`
+      select id
+      from projects
+      where user_id = ${userId} and slug = '${projectSlug}'
+    `);
 
-  if (!project) {
-    return res
-      .status(500)
-      .json({ success: false, message: "Project does not exists" });
+    if (!project) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Project does not exists" });
+    }
+
+    const bankAccount = await db.queryOne(`
+      select id
+      from bank_accounts
+      where user_id = ${userId} and iban = '${iban}'
+    `);
+
+    const dueDate = moment()
+      .add(1, "month")
+      .format("YYYY-MM-DD");
+    let number = 1;
+
+    const lastInvoice = await db.queryOne(`
+      select number
+      from invoices
+      where user_id = ${userId}
+      order by id desc
+      limit 1
+    `);
+
+    if (lastInvoice && lastInvoice.number) {
+      number = parseInt(lastInvoice.number) + 1;
+    }
+
+    const invoiceId = await db.execute(
+      `
+        insert into invoices (
+          user_id, 
+          date, 
+          due_date, 
+          name, 
+          number, 
+          project_id, 
+          rate,
+          bank_account_id,
+          user_address_id
+        )
+        values (?, curdate(), ?, ? , ?, ?, ?, ?, ?)
+      `,
+      [
+        userId,
+        dueDate,
+        name,
+        number,
+        project.id,
+        hourlyRate,
+        bankAccount.id,
+        userAddressId
+      ]
+    );
+
+    const activityInvoices = await db.execute(
+      `
+        update activities
+        set invoice_id = ?
+        where month(start_time) = ?
+        and year(start_time) = ?
+        and project_id = ?
+        and user_id = ?
+      `,
+      [invoiceId, month, year, project.id, userId]
+    );
+
+    res.json({ success: true, invoiceId, activityInvoices });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error on insert",
+      error
+    });
   }
-
-  const bankAccount = await db.queryOne(`
-    select id
-    from bank_accounts
-    where user_id = ${userId} and iban = '${iban}'
-  `);
-
-  const dueDate = moment()
-    .add(1, "month")
-    .format("YYYY-MM-DD");
-  let number = 1;
-
-  const lastInvoice = await db.queryOne(`
-    select number
-    from invoices
-    where user_id = ${userId}
-    order by id desc
-    limit 1
-  `);
-
-  if (lastInvoice && lastInvoice.number) {
-    number = parseInt(lastInvoice.number) + 1;
-  }
-
-  const invoiceId = await db.execute(
-    `
-      insert into invoices (
-        user_id, 
-        date, 
-        due_date, 
-        name, 
-        number, 
-        project_id, 
-        rate,
-        bank_account_id,
-        user_address_id
-      )
-      values (?, curdate(), ?, ? , ?, ?, ?, ?, ?)
-    `,
-    [
-      userId,
-      dueDate,
-      name,
-      number,
-      project.id,
-      hourlyRate,
-      bankAccount.id,
-      userAddressId
-    ]
-  );
-
-  const activityInvoices = await db.execute(
-    `
-      update activities
-      set invoice_id = ?
-      where month(start_time) = ?
-      and year(start_time) = ?
-      and project_id = ?
-      and user_id = ?
-    `,
-    [invoiceId, month, year, project.id, userId]
-  );
-
-  res.json({ success: true, invoiceId, activityInvoices });
 };
