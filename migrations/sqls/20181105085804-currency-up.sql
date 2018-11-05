3112d3caebd2aa677a6cf0a2f6832460eea00495
Loïc Nogues <nogues.loic@gmail.com>
2018-11-05 10:10:24 +0100

    Created currency table

---
+++ migrations/sqls/20181105085804-currency-up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `currencies` (
+  `code` VARCHAR(3) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `sign` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`code`));
---
+++ migrations/sqls/20181105090755-currency-populate-up.sql
@@ -0,0 +1,4 @@
+INSERT INTO `currencies` (`code`, `name`, `sign`)
+VALUES ('EUR', 'Euro', '€'),
+('CAD', 'Canadian dollar', '$'),
+('USD', 'US dollar', '$');
