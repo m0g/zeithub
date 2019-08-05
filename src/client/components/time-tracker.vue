3787460923676022c792618f007b01cef8e225b6
Loïc Nogues <nogues.loic@gmail.com>
2019-08-05 15:40:15 +0200

    Time tracker WIP

--- src/client/components/time-tracker.vue
+++ src/client/components/time-tracker.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
     <h3>Time tracker</h3>
-    <select-project
-      :disabled="isRecording"
-      v-bind:value="projectId"
-      v-on:projectId="projectId = $event"
-    ></select-project>
-    <button :disabled="projectId === 0" v-if="!isRecording" @click="start()">
-      ⏺️
-    </button>
-    <button v-if="isRecording" @click="stop()">⏹️</button>
-    <span>{{ (currentTime - startTime) | duration }}</span>
+    <div>
+      <select-project
+        :disabled="isRecording"
+        v-bind:value="projectId"
+        v-on:projectId="projectId = $event"
+      ></select-project>
+    </div>
+    <div>
+      <input :disabled="isRecording" type="text" v-model="name" />
+    </div>
+    <div>
+      <button :disabled="projectId === 0" v-if="!isRecording" @click="start()">
+        ⏺️
+      </button>
+      <button v-if="isRecording" @click="stop()">⏹️</button>
+      <span>{{ calcDuration() }}</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import moment from 'moment';
+
 import SelectProject from './select-project.vue';
 
 @Component({
@@ -25,10 +34,23 @@ import SelectProject from './select-project.vue';
 export default class TimeTracker extends Vue {
   projectId: number = 0;
   isRecording: boolean = false;
-  time: number = 0;
   interval: number = 0;
   startTime: number = 0;
   currentTime: number = 0;
+  name: string = '';
+  activities: Object[] = [];
+
+  calcDuration() {
+    let timestamp: number = this.currentTime - this.startTime;
+
+    if (this.activities.length > 0) {
+      timestamp = this.activities.reduce((acc: number, activity) => {
+        return acc + (activity.endTime - activity.startTime);
+      }, timestamp);
+    }
+
+    return moment.utc(timestamp).format('HH:mm:ss');
+  }
 
   start() {
     this.isRecording = true;
@@ -38,8 +60,18 @@ export default class TimeTracker extends Vue {
   }
 
   stop() {
+    this.activities.push({
+      projectId: this.projectId,
+      name: this.name,
+      startTime: this.startTime,
+      endTime: this.currentTime
+    });
+
     this.isRecording = false;
     clearInterval(this.interval);
+
+    this.startTime = 0;
+    this.currentTime = 0;
   }
 
   updateCurrentTime() {
