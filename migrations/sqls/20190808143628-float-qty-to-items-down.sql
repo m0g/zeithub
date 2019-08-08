1bd6bfa0d2ffc0d83500339ba5e094f3126f8aa8
Loïc Nogues <nogues.loic@gmail.com>
2019-08-08 16:47:26 +0200

    Qty row is now decimal

---
+++ migrations/sqls/20190808143628-float-qty-to-items-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE items MODIFY COLUMN qty INT(11) NULL;
---
+++ migrations/sqls/20190808143628-float-qty-to-items-up.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+ALTER TABLE items MODIFY COLUMN qty DECIMAL(10,1) NULL;
