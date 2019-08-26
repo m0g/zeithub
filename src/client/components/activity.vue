b807b485a53eb180494a563d930ee595258b7660
Loïc Nogues <nogues.loic@gmail.com>
2019-08-26 15:25:23 +0200

    Delete an activity

--- src/client/components/activity.vue
+++ src/client/components/activity.vue
@@ -1,25 +1,44 @@
 <template>
   <li>
     <span>
-      {{activity.startTime | formatDate('HH:mm')}} - 
-      {{activity.endTime | formatDate('HH:mm')}}
+      {{ activity.startTime | formatDate('HH:mm') }} -
+      {{ activity.endTime | formatDate('HH:mm') }}
     </span>
-    <span>{{activity.name}}</span>
-    <span>{{activity.durationMinutes}}</span>
-    <router-link :to="{ name: 'Project', params: { slug: activity.projectSlug } }">
-      {{activity.projectName}}
+    <span>{{ activity.name }}</span>
+    <span>{{ activity.durationMinutes }}</span>
+    <router-link
+      :to="{ name: 'Project', params: { slug: activity.projectSlug } }"
+    >
+      {{ activity.projectName }}
     </router-link>
+    <button @click="remove()">&#x2718;</button>
   </li>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import http from '../http';
 
 const Props = Vue.extend({
   props: ['activity']
 });
 
 @Component({})
-export default class Activity extends Props {}
+export default class Activity extends Props {
+  async remove() {
+    if (window.confirm('Do you really want to delete this activity?')) {
+      const response = await http(`/api/activities/${this.activity.id}`, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'DELETE'
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        // this.getClients();
+      }
+    }
+  }
+}
 </script>
