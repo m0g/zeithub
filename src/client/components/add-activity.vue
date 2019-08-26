5f9fff172767014181abb0f85deaaf478bbd33fb
Loïc Nogues <nogues.loic@gmail.com>
2019-08-26 14:39:11 +0200

    Add activity form

--- src/client/components/add-activity.vue
+++ src/client/components/add-activity.vue
@@ -3,9 +3,20 @@
     <h3>Add activity</h3>
     <form method="POST" @submit="addActivity">
       <form-errors :errors="errors"></form-errors>
-      <input type="text" placeholder="Name" v-model="name" />
-      <input type="datetime-local" placeholder="Start time" v-model="start" />
-      <input type="datetime-local" placeholder="End time" v-model="end" />
+      <div>
+        <input type="text" placeholder="Name" v-model="name" />
+      </div>
+      <div>
+        <label>Start</label>
+        <input type="date" placeholder="Start date" v-model="startDate" />
+        <input type="time" placeholder="Start time" v-model="startTime" />
+      </div>
+      <div>
+        <label>Stop</label>
+        <input type="date" placeholder="End Date" v-model="endDate" />
+        <input type="time" placeholder="End time" v-model="endTime" />
+      </div>
+      <div>Duration: {{ getDuration() }}</div>
       <select-project
         v-bind:projectId="projectId"
         v-on:projectId="projectId = $event"
@@ -18,6 +29,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import moment from 'moment';
 import http from './../http';
 import FormErrors from './form-errors.vue';
 import SelectProject from './select-project.vue';
@@ -28,8 +40,19 @@ import SelectProject from './select-project.vue';
 export default class AddExpense extends Vue {
   errors: string[] = [];
   name: string = '';
-  start: string = '';
-  end: string = '';
+  startDate: string = '';
+  startTime: string = '';
+  endDate: string = '';
+  endTime: string = '';
+  projectId: number = 0;
+
+  getDuration() {
+    const start = moment(`${this.startDate} ${this.startTime}`);
+    const end = moment(`${this.endDate} ${this.endTime}`);
+    const duration = moment.duration(end.diff(start));
+
+    return duration.asMinutes() || 0;
+  }
 
   async addActivity(e) {
     this.errors = [];
@@ -39,15 +62,39 @@ export default class AddExpense extends Vue {
       this.errors.push('Name is required');
     }
 
-    if (!this.start) {
+    if (!this.startDate) {
+      this.errors.push('Start date is required');
+    }
+
+    if (!this.startTime) {
       this.errors.push('Start time is required');
     }
 
-    if (!this.end) {
+    if (!this.endDate) {
+      this.errors.push('End date is required');
+    }
+
+    if (!this.endTime) {
       this.errors.push('End time is required');
     }
 
     if (this.errors.length === 0) {
+      const response = await http('/api/activities', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: this.name,
+          start: `${this.startDate} ${this.startTime}`,
+          end: `${this.endDate} ${this.endTime}`,
+          projectId: this.projectId
+        })
+      });
+
+      const data = await response.json();
+
+      if (data.success === true) {
+        console.log('done');
+      }
     }
   }
 }
